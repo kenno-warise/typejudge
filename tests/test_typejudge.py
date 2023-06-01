ad1abe7c1dfa26d75875706677e697fce71d3b91
Kenno-Warise <wariken0523@gmail.com>
2023-06-01 13:01:28 +0900

    テストファイルの成功

--- tests/test_typejudge.py
+++ tests/test_typejudge.py
@@ -8,7 +8,7 @@ class JudgeTest(unittest.TestCase):
     def test_judgement(self):
         typecode = judgement('a')
         # データ型の判定
-        self.assertIsInstance(typecode, str)
+        self.assertIsInstance(typecode, type)
 
 
 if __name__ == '__main__':
