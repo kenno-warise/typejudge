8fcd3f8f3424aea905c1696e5c5c71e16565a3c9
Kenno-Warise <wariken0523@gmail.com>
2023-06-01 12:49:44 +0900

    テストファイルの失敗を招く

--- tests/test_typejudge.py
+++ tests/test_typejudge.py
@@ -8,7 +8,7 @@ class JudgeTest(unittest.TestCase):
     def test_judgement(self):
         typecode = judgement('a')
         # データ型の判定
-        self.assertIsInstance(typecode, type)
+        self.assertIsInstance(typecode, str)
 
 
 if __name__ == '__main__':
